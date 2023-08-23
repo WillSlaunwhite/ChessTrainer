7b41ddbf2d106f97ad780863e0345e05ee09cc46
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-08-23 03:19:20 -0700

    basic chess board rendering

--- chess-trainer/src/App.tsx
+++ chess-trainer/src/App.tsx
@@ -1,13 +1,12 @@
-import Chessboard from 'chessboardjsx'
-import './App.css'
+import "./App.scss";
+import ChessboardComponent from "./chessboard/chessboard";
 
 function App() {
-
-  return (
-    <div className="App">
-      <Chessboard position="start"/>
-    </div>
-  )
+	return (
+		<div className="flex justify-center items-center h-screen w-screen bg-gray-800">
+			<ChessboardComponent initialPosition="start" />
+		</div>
+	);
 }
 
-export default App
+export default App;

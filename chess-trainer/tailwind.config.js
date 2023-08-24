ef866e146a52e5006dc09128c6d198f7248cae25
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-08-23 20:24:16 -0700

    initial dependencies and configs

---
+++ chess-trainer/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+	darkMode: "false",
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};

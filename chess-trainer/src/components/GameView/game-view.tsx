50e628c3f7f9242bb38a38eabe147d3e1232726c
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-09-03 02:27:30 -0700

    committing before removing unused for build

--- chess-trainer/src/components/GameView/game-view.tsx
+++ chess-trainer/src/components/GameView/game-view.tsx
@@ -3,56 +3,8 @@ import ChessboardComponent from "../chessboard/chessboard";
 import ExplanationComponent from "../text/explanation";
 import MoveContainer from "./move-container";
 import SummaryComponent from "./summary";
-import { Question } from "../../models/interfaces";
+import { italianGameLines, questions } from "../../models/constants";
 
-
-const questions: Question[] = [
-	{
-		fen: "r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4",
-		correctMove: "c3",
-		correctExplanation: "This move prepares d4 and gains central space.",
-		incorrectExplanation: "c3 is the best move because it prepares d4 and gains central space.",
-	},
-	{
-		fen: "r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/2P2N2/PP1P1PPP/RNBQK2R b KQkq - 0 4",
-		correctMove: "Nf6",
-		correctExplanation: "Develops a piece and attacks the e4 pawn.",
-		incorrectExplanation: "Nf6 is the best move because it develops a piece and attacks the e4 pawn.",
-	},
-	{
-		fen: "r1bqk2r/pppp1ppp/2n2n2/2b1p3/2B1P3/2P2N2/PP1P1PPP/RNBQK2R w KQkq - 1 5",
-		correctMove: "d3",
-		correctExplanation: "This move supports the e4 pawn and prepares to develop the dark square bishop.",
-		incorrectExplanation: "d3 is the best move because it supports the e4 pawn and prepares to develop the dark square bishop.",
-	},
-	{
-		fen: "r1bqk2r/pppp1ppp/2n2n2/2b1p3/2B1P3/2PP1N2/PP3PPP/RNBQK2R b KQkq - 0 5",
-		correctMove: "d6",
-		correctExplanation: "This move supports the e5 pawn and prepares to develop the dark square bishop.",
-		incorrectExplanation: "d6 is the best move because it supports the e5 pawn and prepares to develop the dark square bishop.",
-	},
-	{
-		fen: "r1bqk2r/ppp2ppp/2np1n2/2b1p3/2B1P3/2PP1N2/PP3PPP/RNBQK2R w KQkq - 0 6",
-		correctMove: "O-O",
-		correctExplanation:
-			"Castling is always a good idea if there are no immediate tactics or threats. It brings the king to safety and connects the rooks.",
-		incorrectExplanation: "Castling is the best move because it brings the king to safety and connects the rooks.",
-	},
-	{
-		fen: "r1bq1rk1/ppp2ppp/2np1n2/2b1p3/2B1P3/2PP1N2/PP3PPP/RNBQ1RK1 b - - 1 6",
-		correctMove: "O-O",
-		correctExplanation:
-			"Castling is always a good idea if there are no immediate tactics or threats. It brings the king to safety and connects the rooks.",
-		incorrectExplanation: "Castling is the best move because it brings the king to safety and connects the rooks.",
-	},
-	{
-		fen: "r1bq1rk1/ppp2ppp/2np1n2/2b1p3/2B1P3/2PP1N2/PP3PPP/RNBQ1RK1 w - - 2 7",
-		correctMove: "Nbd2",
-		correctExplanation: "Develops a piece, supports e4, and prepares to reroute the knight to a better square.",
-		incorrectExplanation:
-			"Nbd2 is the best move because it develops a piece, supports e4, and prepares to reroute the knight to a better square.",
-	},
-];
 
 interface GameViewProps {
 	fen: string;
--- chess-trainer/src/components/chessboard/chessboard.tsx
+++ chess-trainer/src/components/chessboard/chessboard.tsx
@@ -1,7 +1,7 @@
 import { Chess, Square } from "chess.js";
 import Chessboard from "chessboardjsx";
 import { useEffect, useRef, useState } from "react";
-
+import { italianGameLines, italianGameMainLine } from "../../models/constants";
 
 interface ChessboardProps {
 	fen: string;
@@ -16,17 +16,17 @@ const ChessboardComponent: React.FC<ChessboardProps> = ({
 	setFen,
 	darkSquareColor = "#7d5426",
 	lightSquareColor = "#e6d9bc",
-	onMove
-	}) => {
+	onMove,
+}) => {
 	const chess = useRef(new Chess());
 	const [squareStyles, setSquareStyles] = useState({});
 	const [selectedSquare, setSelectedSquare] = useState<string | null>(null);
+	const [moveIndex, setMoveIndex] = useState(0);
 
 
 	useEffect(() => {
 		chess.current.load(fen);
 	}, [fen]);
-
 
 	const handleClick = (square: string) => {
 		if (selectedSquare === null) {
@@ -41,28 +41,58 @@ const ChessboardComponent: React.FC<ChessboardProps> = ({
 		}
 	};
 
-	const handleMove = (move: {from: string, to: string}) => {
+	const handleMove = (move: { from: string; to: string }) => {
 		const { from, to } = move;
 		const moves = chess.current.moves({ square: from as Square, verbose: true });
 
+		const newChessInstance = new Chess();
+		newChessInstance.load(chess.current.fen());
+
 		for (let i = 0; i < moves.length; i++) {
 			if (moves[i].to === to) {
-				chess.current.move({ from, to, promotion: "q" });
-				setFen(chess.current.fen());
+				if (newChessInstance.move({ from, to, promotion: "q" })) {
+					console.log("New instance history:", newChessInstance.history({ verbose: true }));
+				}
+
+				// console.log("Before move:", chess.current.history({ verbose: true }));
+				// chess.current.move({ from, to, promotion: "q" });
+				// console.log("After move:", chess.current.history({ verbose: true }));
+				setFen(newChessInstance.fen());
 				setSquareStyles({
 					[from]: { backgroundColor: "rgba(255, 255, 0, 0.4)" },
 					[to]: { backgroundColor: "rgba(255, 255, 0, 0.4)" },
 				});
-				console.log(chess.current.history({ verbose: true }));
-				console.log(chess.current.fen());
+				console.log(newChessInstance.history({ verbose: true }));
+				console.log(newChessInstance.fen());
 				onMove(moves[i].san);
 				break;
 			}
 		}
+
+		if (newChessInstance.turn() === "b") {
+			setTimeout(() => {
+				console.log(newChessInstance.history({ verbose: true }));
+
+				const whiteMoveIndex = moveIndex;
+				console.log(whiteMoveIndex);
+				const blackMoveSan = italianGameMainLine.black[whiteMoveIndex];
+				console.log(blackMoveSan);
+				const possibleMoves = newChessInstance.moves({ verbose: true });
+				console.log(possibleMoves);
+
+				const blackMove = possibleMoves.find((move) => move.san === blackMoveSan);
+
+				if (blackMove) {
+					const blackMoveResult = newChessInstance.move(blackMove);
+					if (blackMoveResult) {
+						console.log(blackMoveResult);
+						setFen(newChessInstance.fen());
+					}
+					setMoveIndex(prevIndex => prevIndex + 1);
+				}
+			}, 500);
+		}
 	};
-
-
-
 
 	const calcWidth = ({ screenWidth }: { screenWidth: number }) => {
 		if (screenWidth < 500) {
@@ -84,7 +114,9 @@ const ChessboardComponent: React.FC<ChessboardProps> = ({
 				lightSquareStyle={{ backgroundColor: lightSquareColor }}
 				calcWidth={calcWidth}
 				position={fen}
-				onDrop={(move: { sourceSquare: string; targetSquare: string }) => handleMove({from: move.sourceSquare, to: move.targetSquare})}
+				onDrop={(move: { sourceSquare: string; targetSquare: string }) =>
+					handleMove({ from: move.sourceSquare, to: move.targetSquare })
+				}
 				onSquareClick={handleClick}
 				draggable={true}
 			/>

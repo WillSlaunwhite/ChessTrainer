6e930c7ef64536dde5607b271634d69e0eae9406
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-09-07 23:57:43 -0700

    able to parse PGNs to game entity format

--- ChessTrainer/src/main/kotlin/com/chesstrainer/utils/ChessPGNParser.kt
+++ ChessTrainer/src/main/kotlin/com/chesstrainer/utils/ChessPGNParser.kt
@@ -1,7 +1,13 @@
 package com.chesstrainer.utils
 
 import com.chesstrainer.entities.ChessGame
+import com.chesstrainer.entities.MasterGame
+import com.chesstrainer.entities.Opening
+import com.chesstrainer.enums.Result
 import java.io.File
+import java.lang.Double
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 fun main() {
     val games = readAndParsePGN("/Users/tristan/Projects/ChessTrainer/pgn-files/test.pgn")
@@ -11,9 +17,12 @@ fun main() {
     }
 }
 
+fun isGameResult(line: String): Boolean {
+    return line.contains(Regex("[0-1]-[0-1]|1/2-1/2"))
+}
 
-fun parsePGN(pgnContent: List<String>): List<ChessGame> {
-    val games = mutableListOf<ChessGame>()
+fun parsePGN(pgnContent: List<String>): List<MasterGame> {
+    val games = mutableListOf<MasterGame>()
     val currentGameMetadata = mutableMapOf<String, String>()
     val currentMoves = mutableListOf<String>()
 
@@ -23,88 +32,79 @@ fun parsePGN(pgnContent: List<String>): List<ChessGame> {
                 val key = line.substringAfter("[").substringBefore(" ").trim()
                 val value = line.substringAfter("\"").substringBeforeLast("\"").trim()
                 currentGameMetadata[key] = value
-                if (key == "ECO") {
-                    println("ECO ECO ECO")
-                }
             }
 
-            line.contains(Regex("[0-9]")) -> {
-                val allWords = line.split(" ")
-                allWords.forEach { word ->
-                    if(word.matches(Regex(" [0-1]-[0-1]|1/2-1/2"))) {
-                        println("RESULT RESULT RESULT $word")
+            isGameResult(line) -> {
+                val movesWithResult = line.trim().split(" ")
+                val result = movesWithResult.last() // The last element is the game result
+                val movesWithoutResult = movesWithResult.dropLast(1).joinToString(" ")
+
+                val moveMatcher =
+                    Regex("(\\d+\\.)\\s*([\\w\\+\\#\\-\\=]+)\\s*([\\w\\+\\#\\-\\=]*)").findAll(movesWithoutResult)
+
+                for (match in moveMatcher) {
+                    currentMoves.add(match.groupValues[1] + match.groupValues[2])
+                    if (match.groupValues[3].isNotEmpty()) {
+                        currentMoves.add(match.groupValues[1] + match.groupValues[3])
                     }
                 }
-            }
 
-//            line.contains(Regex("[01]-[01]|1/2-1/2")) -> {
-//                currentMoves.add(line.trim())
-//                val game: ChessGame =
-//                    ChessGame(
-//                        event = currentGameMetadata["Event"],
-//                        site = currentGameMetadata["Site"],
-//                        date = currentGameMetadata["Date"],
-//                        round = currentGameMetadata["Round"],
-//                        white = currentGameMetadata["White"] ?: "",
-//                        black = currentGameMetadata["Black"] ?: "",
-//                        result = line.trim(),
-//                        whiteElo = currentGameMetadata["WhiteElo"] ?: "",
-//                        blackElo = currentGameMetadata["BlackElo"] ?: "",
-//                        eco = currentGameMetadata["ECO"] ?: "",
-//                        moves = currentMoves
-//                    )
-//                games.add(
-//                    game
-//                )
-//                currentGameMetadata.clear()
-//                currentMoves.clear()
-//            }
 
-            else -> {
-                val allMoves = line.split(" ")
-                if (line.contains(Regex(" [0-1]-[0-1]")) || line.contains(Regex("1/2-1/2"))) {
-                    allMoves.forEach { word ->
-                        if (!word.contains(Regex("[0-1]-[0-1]")) && !word.contains("1/2-1/2") && !word.contains(Regex("^\\d+\\."))) {
-                            currentMoves.add(word)
-                        } else {
-                            println("MOVES BLOCK MOVES BLOCK MOVES BLOCK $currentMoves")
-                            println("MOVES BLOCK LINE MOVES BLOCK LINE MOVES BLOCK LINE $line")
-                            currentMoves.add(line.trim())
-                        }
-                    }
-                } else {
-                    println("MATCH MATCH MATCH $currentGameMetadata")
-                    println("LINE LINE LINE $line")
-                    val game: ChessGame =
-                        ChessGame(
-                            event = currentGameMetadata["Event"],
-                            site = currentGameMetadata["Site"],
-                            date = currentGameMetadata["Date"],
-                            round = currentGameMetadata["Round"],
-                            white = currentGameMetadata["White"] ?: "",
-                            black = currentGameMetadata["Black"] ?: "",
-                            result = line.trim(),
-                            whiteElo = currentGameMetadata["WhiteElo"] ?: "",
-                            blackElo = currentGameMetadata["BlackElo"] ?: "",
-                            eco = currentGameMetadata["ECO"] ?: "",
-                            moves = currentMoves
-                        )
-                    println("GAME GAME GAME $game")
-                    games.add(
-                        game
+//                val id: Long,
+//                val event: String? = null,
+//                val site: String = "?",
+//                val date: String? = null,
+//                val round: Int? = null,
+//                val white: String,
+//                val black: String,
+//                val whiteElo: Int? = null,
+//                val blackElo: Int? = null,
+//                val result: Result,
+//                val eco: String,
+//                val moves: List<String>,
+//                @ManyToOne @JoinColumn(name = "opening_id")
+//                val opening: Opening,
+
+                if (result != " ") {
+                    val res =
+                        if (result.equals("1-0")) Result.WHITE else if (result.equals("0-1")) Result.BLACK else Result.DRAW
+                    val game = MasterGame(
+                        event = currentGameMetadata["Event"],
+                        site = currentGameMetadata["Site"],
+                        date = currentGameMetadata["Date"],
+                        round = currentGameMetadata["Round"] ?: "",
+                        white = currentGameMetadata["White"] ?: "",
+                        black = currentGameMetadata["Black"] ?: "",
+                        result = res,
+                        whiteElo = if (currentGameMetadata["WhiteElo"] != "") Integer.valueOf(currentGameMetadata["WhiteElo"]) else null,
+                        blackElo = if (currentGameMetadata["BlackElo"] != "") Integer.valueOf(currentGameMetadata["BlackElo"]) else null,
+                        eco = currentGameMetadata["ECO"] ?: "",
+                        moves = if (currentMoves.toList().size < 20) currentMoves.toList() else currentMoves.toList()
+                            .subList(0, 20)
                     )
+                    games.add(game)
                     currentGameMetadata.clear()
                     currentMoves.clear()
-
                 }
             }
+
+            else -> {
+                val moveMatcher = Regex("(\\d+\\.)\\s*([\\w\\+\\#\\-\\=]+)\\s*([\\w\\+\\#\\-\\=]*)").findAll(line)
+                for (match in moveMatcher) {
+                    currentMoves.add(match.groupValues[1] + match.groupValues[2])
+                    if (match.groupValues[3].isNotEmpty()) {
+                        currentMoves.add(match.groupValues[1] + match.groupValues[3])
+                    }
+                }
+            }
+
         }
     }
     println("end $games")
     return games
 }
 
-public fun readAndParsePGN(filePath: String): List<ChessGame> {
+fun readAndParsePGN(filePath: String): List<MasterGame> {
     val lines = File(filePath).readLines()
     return parsePGN(lines)
 }

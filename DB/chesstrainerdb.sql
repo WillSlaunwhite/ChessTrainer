a16d0a4b826613ee76dd012dfa23423cdc345c80
Tristan Slaunwhite <williamslaunwhite@gmail.com>
2023-09-24 00:18:45 -0700

    added variation to schema, changed inserts

--- DB/chesstrainerdb.sql
+++ DB/chesstrainerdb.sql
@@ -46,10 +46,24 @@ CREATE TABLE IF NOT EXISTS `opening` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `description` TEXT NOT NULL,
-  `moves_sequence` VARCHAR(1000) NOT NULL,
+  `base_moves_sequence` VARCHAR(1000) NOT NULL,
   `difficulty_level` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC)
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `variations`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `variations`;
+
+CREATE TABLE IF NOT EXISTS `variations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `moves_sequence` VARCHAR(1000) NOT NULL,
+  `opening_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`opening_id`) REFERENCES `opening` (`id`)
 ) ENGINE = InnoDB;
 
 
@@ -136,7 +150,10 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 INSERT INTO user(id, username, password, role, email, registration_date, last_login) VALUES (1, 'chess_user', '$2a$10$5C451TAuy.Dd/lx/QFnVsOwZ27/SbZTUH3IuNLy1ChQXMybJHMHa2', "user", "chess_user@chesstrainer.com", NOW(), NOW());
 
-INSERT INTO opening(id, name, description, moves_sequence, difficulty_level) VALUES 
-(1, 'Italian Game - Main Line', 'The Italian Game begins with 1.e4 e5 2.Nf3 Nc6 3.Bc4, emphasizing rapid development, central control, and a Kingside presence.', '1.e4 e5 2.Nf3 Nc6 3.Bc4', 'Intermediate'),
-(2, 'Two Knights Defense', 'The Two Knights Defense is a dynamic and aggressive response to the Italian Game. After 1.e4 e5 2.Nf3 Nc6 3.Bc4, Black immediately challenges White\'s central pawn with 3...Nf6. This opening often leads to sharp play with both sides having chances for a kingside attack. It\'s named for the two knights that are developed by move 3, and it has been a favorite of many top players throughout history.', '1.e4 e5 2.Nf3 Nc6 3.Bc4 Nf6', 'Intermediate'),
-(3, 'Hungarian Defense', 'The Hungarian Defense is a solid and somewhat passive response to the Italian Game. After 1.e4 e5 2.Nf3 Nc6 3.Bc4, Black opts for the quiet 3...Be7, preparing to castle kingside and maintain a flexible pawn structure. This defense prioritizes safety and development over immediate confrontation, making it a suitable choice for beginners or those looking for a less combative game.', '1.e4 e5 2.Nf3 Nc6 3.Bc4 Be7', 'Beginner');
+INSERT INTO opening(id, name, description, base_moves_sequence, difficulty_level) VALUES 
+(1, 'Italian Game', 'The Italian Game begins with 1.e4 e5 2.Nf3 Nc6 3.Bc4, emphasizing rapid development, central control, and a Kingside presence.', '1.e4 e5 2.Nf3 Nc6 3.Bc4', 'Intermediate');
+
+INSERT INTO variations(id, name, moves_sequence, opening_id) VALUES 
+(1, 'Main Line', '', 1),
+(2, 'Two Knights Defense', 'Nf6', 1),
+(3, 'Hungarian Defense', 'Be7', 1);
